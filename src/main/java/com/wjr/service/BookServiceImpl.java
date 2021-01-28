9bc48a7bf318fe8ad842e393188a2aea361d9f80
ookurumi-wjr <68881811+ookurumi-wjr@users.noreply.github.com>
2021-01-28 16:04:42 +0800

    Initial commit

---
+++ src/main/java/com/wjr/service/BookServiceImpl.java
@@ -0,0 +1,35 @@
+package com.wjr.service;
+
+import com.wjr.dao.BookMapper;
+import com.wjr.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
